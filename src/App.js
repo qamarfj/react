398470fc9cee20d30c615d81c0adc2cca38b21b6
qamarfj <qamarfj@hotmail.com>
2021-04-04 19:55:21 +0200

    path added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import pic from "./pic.jpg";
-
+var path = require("path");
+var pic3 = path.basename("/react/assets/pic.jpg");
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <img src={pic}></img>
         <img src="./pic.jpg" alt="2nd pic"></img>
+        <img src={pic3} alt="3rd pic"></img>
       </header>
     </div>
   );
