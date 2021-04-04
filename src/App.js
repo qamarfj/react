fafc8f089e131b83f3dd81200d93cf65ff961104
qamarfj <qamarfj@hotmail.com>
2021-04-04 19:24:27 +0200

    ad pic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import pic from "./pic.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={pic}></img>
       </header>
     </div>
   );
